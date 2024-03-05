adfa707e0683d0f721d40549f52031389942367d
Massimiliano Arena <milikkan5000@gmail.com>
2024-03-05 09:51:57 +0100

    modifica

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div>
+
+        </div>
+        <div>
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </div>
+      </div>
+      <h1>Massimiliano Arena</h1>
+      <p className="read-the-docs">
+        Coming Soon...
+      </p>
+    </>
+  )
+}
+
+export default App
